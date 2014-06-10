ee76fee1d39462bb1a88ffd3ec883affa2479a75
MISS <miss@miss-MS-7549.(none)>
2014-06-10 23:28:47 +0800

    Fix lddbus module compile based on kernel 3.11.

--- lddbus/lddbus.c
+++ lddbus/lddbus.c
@@ -30,14 +30,15 @@ static char *Version = "$Revision: 1.9 $";
 /*
  * Respond to hotplug events.
  */
-static int ldd_hotplug(struct device *dev, char **envp, int num_envp,
-		char *buffer, int buffer_size)
+//static int ldd_hotplug(struct device *dev, char **envp, int num_envp,
+//		char *buffer, int buffer_size)
+static int ldd_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
-	envp[0] = buffer;
-	if (snprintf(buffer, buffer_size, "LDDBUS_VERSION=%s",
-			    Version) >= buffer_size)
+	env->envp[0] = env->buf;
+	if (snprintf(env->buf, env->buflen, "LDDBUS_VERSION=%s",
+			    Version) >= env->buflen)
 		return -ENOMEM;
-	envp[1] = NULL;
+	env->envp[1] = NULL;
 	return 0;
 }
 
@@ -46,7 +47,7 @@ static int ldd_hotplug(struct device *dev, char **envp, int num_envp,
  */
 static int ldd_match(struct device *dev, struct device_driver *driver)
 {
-	return !strncmp(dev->bus_id, driver->name, strlen(driver->name));
+	return !strncmp(dev->init_name, driver->name, strlen(driver->name));
 }
 
 
@@ -59,7 +60,7 @@ static void ldd_bus_release(struct device *dev)
 }
 	
 struct device ldd_bus = {
-	.bus_id   = "ldd0",
+	.init_name   = "ldd0",
 	.release  = ldd_bus_release
 };
 
@@ -70,7 +71,8 @@ struct device ldd_bus = {
 struct bus_type ldd_bus_type = {
 	.name = "ldd",
 	.match = ldd_match,
-	.hotplug  = ldd_hotplug,
+	//.hotplug  = ldd_hotplug,
+	.uevent = ldd_uevent,
 };
 
 /*
@@ -102,7 +104,8 @@ int register_ldd_device(struct ldd_device *ldddev)
 	ldddev->dev.bus = &ldd_bus_type;
 	ldddev->dev.parent = &ldd_bus;
 	ldddev->dev.release = ldd_dev_release;
-	strncpy(ldddev->dev.bus_id, ldddev->name, BUS_ID_SIZE);
+	//strncpy(ldddev->dev.bus_id, ldddev->name, BUS_ID_SIZE);
+	ldddev->dev.init_name = ldddev->name;
 	return device_register(&ldddev->dev);
 }
 EXPORT_SYMBOL(register_ldd_device);
@@ -136,7 +139,7 @@ int register_ldd_driver(struct ldd_driver *driver)
 	if (ret)
 		return ret;
 	driver->version_attr.attr.name = "version";
-	driver->version_attr.attr.owner = driver->module;
+	//driver->version_attr.attr.owner = driver->module;
 	driver->version_attr.attr.mode = S_IRUGO;
 	driver->version_attr.show = show_version;
 	driver->version_attr.store = NULL;
