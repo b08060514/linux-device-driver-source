986c18bfc1100213a2600b262af92c9ae06ac80a
MISS <miss@miss-MS-7549.(none)>
2014-07-20 13:23:07 +0800

    Fix sculld compile err on linux-3.12, but there still are some function problems: 1,module sculld uses symbles of lddbus, although i have ismode lddbus, 	but it still alert "no symbol version for " while insmoding sculld. 2,it warns "module verification failed: signature and/or required key missing " when insmod lddbus.

--- sculld/mmap.c
+++ sculld/mmap.c
@@ -15,8 +15,9 @@
  * $Id: _mmap.c.in,v 1.13 2004/10/18 18:07:36 corbet Exp $
  */
 
-#include <linux/config.h>
+//#include <linux/config.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
 #include <linux/mm.h>		/* everything */
 #include <linux/errno.h>	/* error codes */
@@ -62,7 +63,7 @@ struct page *sculld_vma_nopage(struct vm_area_struct *vma,
 {
 	unsigned long offset;
 	struct sculld_dev *ptr, *dev = vma->vm_private_data;
-	struct page *page = NOPAGE_SIGBUS;
+	struct page *page = NULL;
 	void *pageptr = NULL; /* default to "missing" */
 
 	down(&dev->sem);
@@ -96,7 +97,7 @@ struct page *sculld_vma_nopage(struct vm_area_struct *vma,
 struct vm_operations_struct sculld_vm_ops = {
 	.open =     sculld_vma_open,
 	.close =    sculld_vma_close,
-	.nopage =   sculld_vma_nopage,
+	//.nopage =   sculld_vma_nopage,
 };
 
 
@@ -110,7 +111,7 @@ int sculld_mmap(struct file *filp, struct vm_area_struct *vma)
 
 	/* don't do anything here: "nopage" will set up page table entries */
 	vma->vm_ops = &sculld_vm_ops;
-	vma->vm_flags |= VM_RESERVED;
+	//vma->vm_flags |= VM_RESERVED;
 	vma->vm_private_data = filp->private_data;
 	sculld_vma_open(vma);
 	return 0;
